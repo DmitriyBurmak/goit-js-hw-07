c0e6b89df1c8083d939aac5c8f4fe86e87ca8d5a
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-03-04 12:27:17 +0200

    performance of the task-1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach(category => {
+  const title = category.querySelector('h2').textContent;
+  const elementsCount = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
